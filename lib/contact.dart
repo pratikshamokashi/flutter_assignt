702ca3e9c05ef6b78de132c5c53f7f5cf2a4f9d2
Pratiksha <pratiksha.mokashi@neosoftmail.com>
2021-01-19 11:12:54 +0530

    contact list

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class Contact extends StatefulWidget {
   @override
   _ContactState createState() => _ContactState();
@@ -10,8 +12,74 @@ class _ContactState extends State<Contact> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Contact Us"),
+        title: new Text("Contact List"),
+      ),
+      body :UserList()
+    );
+  }
+}
+
+class UserList extends StatelessWidget{
+  final String apiUrl = "https://randomuser.me/api/?results=10";
+
+  Future<List<dynamic>> fetchUsers() async {
+
+    var result = await http.get(apiUrl);
+    return json.decode(result.body)['results'];
+
+  }
+
+  String _name(dynamic user){
+    return user['name']['title'] + " " + user['name']['first'] + " " +  user['name']['last'];
+
+  }
+
+  String _location(dynamic user){
+    return user['location']['country'];
+  }
+
+  String _age(Map<dynamic, dynamic> user){
+    return "Age: " + user['dob']['age'].toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: FutureBuilder<List<dynamic>>(
+          future: fetchUsers(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if(snapshot.hasData){
+              print(_age(snapshot.data[0]));
+              return ListView.builder(
+                  padding: EdgeInsets.all(8),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index){
+                    return
+                      Card(
+                        child: Column(
+                          children: <Widget>[
+                            ListTile(
+                              leading: CircleAvatar(
+                                  radius: 30,
+                                  backgroundImage: NetworkImage(snapshot.data[index]['picture']['large'])),
+                              title: Text(_name(snapshot.data[index])),
+                              subtitle: Text(_location(snapshot.data[index])),
+                              trailing: Text(_age(snapshot.data[index])),
+                            )
+                          ],
+                        ),
+                      );
+                  });
+            }else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+
+
+        ),
       ),
     );
   }
+
 }
